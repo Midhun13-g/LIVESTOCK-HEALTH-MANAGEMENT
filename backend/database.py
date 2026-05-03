59c7ad1b855fd5638933510f35af5e0f9e57ceb3
Midhun13-g <727723eucd032@skcet.ac.in>
2026-05-03 23:15:28 +0530

    fixed API on database

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI")
+
+if not MONGO_URI:
+    raise ValueError("MONGO_URI is not set in environment variables")
 client = MongoClient(MONGO_URI)
 db = client["Livestock"]
 
